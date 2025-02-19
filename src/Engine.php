95b7710c8f366ac7be28abd488e23195cd43fcc2
evgeniy <uhvatkin.e@gigwork.ru>
2025-02-19 15:24:55 +0300

    fix lint

--- src/Engine.php
+++ src/Engine.php
@@ -7,14 +7,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use function cli\prompt;
 use function cli\line;
 
-function engine(array $numbers, string $name, string $introduction):void
+function engine(array $numbers, string $name, string $introduction): void
 {
     line($introduction);
     foreach ($numbers as $question => $number) {
         $answer = prompt("Question: $question\nYour answer");
         if ($answer !== $number) {
-		line("{$answer} is wrong answer ;(. Correct answer was {$number}");
-			line("Let's try again, {$name}!");
+            line("{$answer} is wrong answer ;(. Correct answer was {$number}");
+            line("Let's try again, {$name}!");
             return;
         } else {
             line('Correct!');
