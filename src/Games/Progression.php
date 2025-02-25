f5fd8602db736f0dae4bc7479c5946e6d75cd8ef
evgeniy <uhvatkin.e@gigwork.ru>
2025-02-25 12:14:31 +0300

    fix define function in progression

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -11,33 +11,32 @@ function greetingProgression()
         $name = welcome();
         setProgression($name);
 }
-
+function getProgression()
+{
+    $numers = [];
+    for ($i = 0; $i < 3; $i++) {
+        $arrProg = [];
+        $iter = rand(1, 5);
+        $quantity = rand(5, 10);
+        $a = 0;
+        $b = 0;
+        for ($j = 1; $j < $quantity; $j++) {
+            $val = $a + $iter;
+            $b = $val;
+            $a = $b;
+            $arrProg[$val] = $val;
+        }
+        $quantity !== 10 ? $f = (10 - $quantity) * $iter : $f = 10 * $iter;
+        $replacement = array($f => "..");
+        $finalArrProg = array_replace($arrProg, $replacement);
+        $key = implode(' ', $finalArrProg);
+        $numers[$key] = (string) $f;
+    }
+        return $numers;
+}
 function setProgression(string $name)
 {
     $introduction = 'What number is missing in the progression?';
-    function getProgression()
-    {
-        $numers = [];
-        for ($i = 0; $i < 3; $i++) {
-            $arrProg = [];
-            $iter = rand(1, 5);
-            $quantity = rand(5, 10);
-            $a = 0;
-            $b = 0;
-            for ($j = 1; $j < $quantity; $j++) {
-                $val = $a + $iter;
-                $b = $val;
-                $a = $b;
-                $arrProg[$val] = $val;
-            }
-            $quantity !== 10 ? $f = (10 - $quantity) * $iter : $f = 10 * $iter;
-            $replacement = array($f => "..");
-            $finalArrProg = array_replace($arrProg, $replacement);
-            $key = implode(' ', $finalArrProg);
-            $numers[$key] = (string) $f;
-        }
-            return $numers;
-    }
     $numbers = getProgression();
     conversation($numbers, $name, $introduction);
 }
