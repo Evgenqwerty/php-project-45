9e9d5f84b539b1048bc3c97a94a89ab45b432c95
evgeniy <uhvatkin.e@gigwork.ru>
2025-03-02 12:52:21 +0300

    linter makes me put spaces

--- src/Engine.php
+++ src/Engine.php
@@ -9,14 +9,14 @@ function gameProcess(array $numbers, string $name, string $introduction): void
 {
     line($introduction);
     foreach ($numbers as $question => $number) {
-            $askQuestion = line("Question: $question");
-            $answer = prompt("Your answer");
+        $askQuestion = line("Question: $question");
+        $answer = prompt("Your answer");
         if ($answer !== $number) {
             line("{$answer} is wrong answer ;(. Correct answer was {$number}");
             line("Let's try again, {$name}!");
             return;
         }
-            line('Correct!');
+        line('Correct!');
     }
     line("Congratulations, $name!");
 }
